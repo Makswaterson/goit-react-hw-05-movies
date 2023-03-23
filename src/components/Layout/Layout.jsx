bac0f7ada57ef667e4b21e22f0e8688d9feed2a6
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-03-23 23:18:55 +0200

    fix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Loader } from '../Loader/Loader';
 import { Container, Header, Nav, NavLinks } from './Layout.styled';
 
